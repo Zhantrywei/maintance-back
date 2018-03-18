3ea9bd6b0678d4825e8ff1118cab6ed78c8e4bf3
zhantrywei <844945330@qq.com>
2018-03-18 22:36:53 +0800

    修改用户模块

--- routes/users.js
+++ routes/users.js
@@ -66,6 +66,14 @@ router.post('/login', function (req, res, next) {
               path: '/',
               maxAge: 1000 * 60 * 60
             })
+            res.cookie("stuName", doc.stuName, {
+              path: '/',
+              maxAge: 1000 * 60 * 60
+            })
+            res.cookie("roleName", doc.roleName, {
+              path: '/',
+              maxAge: 1000 * 60 * 60
+            })
             res.json({
               status: '1',
               msg: '登录成功',
@@ -179,63 +187,129 @@ router.post('/register', function (req, res, next) {
   })
 
 })
- 
+
+/* POST users checkstuid. */
+router.post('/checkstuid', function (req, res, next) {
+  console.log("checkstuid: ", req.body.stuId);
+  User.findOne({ "stuId": req.body.stuId }, function (err, doc) {
+    if (err) {
+      res.json({
+        status: '0',
+        msg: err.message
+      });
+    } else {
+      if (doc) {
+        res.json({
+          status: '0',
+          msg: "该用户已经存在"
+        })
+      } else {
+        res.json({
+          status: '1',
+          msg: "该用户可以被注册"
+        })
+      }
+    }
+  })
+})
+
 
 /* POST files. */
 router.post('/upload', function (req, res, next) {
+  console.log("req.body: ",req);
   var form = new formidable.IncomingForm();
-  form.uploadDir = path.join(__dirname+'/../', 'tmp'); //文件保存的临时目录为当前项目下的tmp文件夹  
-  form.maxFieldsSize = 3 * 1024 * 1024; //大小限制为最大3M    
+  form.uploadDir = path.join(__dirname + '/../', 'tmp'); //文件保存的临时目录为当前项目下的tmp文件夹
+  if (!fs.existsSync(form.uploadDir)) {
+    fs.mkdir(form.uploadDir);
+  }
+  form.maxFieldsSize = 500 * 1024; //大小限制为最大500k
   form.keepExtensions = true; //使用文件的原扩展名  
   form.parse(req, function (err, fields, file) {
     var filePath = '';
-    console.log("file: ",file);
-    //如果提交文件的form中将上传文件的input名设置为file，就从file中取上传文件。否则取for in循环第一个上传的文件。  
-    if (file.file) {
-      filePath = file.file.path;
-    } else {
-      for (var key in file) {
-        if (file[key].path && filePath === '') {
-          filePath = file[key].path;
-          break;
-        }
-      }
-    }
-    //文件移动的目录文件夹，不存在时创建目标文件夹  
-    var targetDir = path.join(__dirname+'/../public/images/', 'user');
-    if (!fs.existsSync(targetDir)) {
-      fs.mkdir(targetDir);
-    }
+    console.log("file: ", file);
+    //获取文件拓展名
+    console.log(".的位置： ",filePath.lastIndexOf('.'))
     var fileExt = filePath.substring(filePath.lastIndexOf('.'));
-    //判断文件类型是否允许上传  
+    //判断文件拓展名是否符合要求
     if (('.jpg.jpeg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
       var err = new Error('此文件类型不允许上传');
       res.json({
-        code: -1,
+        status: -1,
         message: '此文件类型不允许上传'
       });
     } else {
-      //以当前时间戳对上传文件进行重命名  
+      //以当前时间戳对上传文件进行重命名
       var fileName = new Date().getTime() + fileExt;
-      var targetFile = path.join(targetDir, fileName);
-      //移动文件  
-      fs.rename(filePath, targetFile, function (err) {
-        if (err) {
-          console.info(err);
-          res.json({
-            code: -1,
-            message: '操作失败'
-          });
-        } else {
-          //上传成功，返回文件的相对路径  
-          var fileUrl = '/upload/' + fileName;
-          res.json({
-            code: 0,
-            fileUrl: fileUrl
-          });
-        }
-      });
     }
+    //提交的文件的form中上传文件的input名分别为IDCardImg和StuCardImg，从当中获取他们的各自信息
+    if (file.IDCardImg) {
+      console.log("fileName: ",fileName);
+      console.log("fileExt: ",fileExt);
+      filePath = file.IDCardImg.path;
+      res.json({
+        status: '0',
+        msg: 'http://localhost:3000/public/images/user/'+fileName
+      })
+    } else if (file.StuCardImg) {
+      filePath = file.StuCardImg.path;
+      res.json({
+        status: '0',
+        msg: 'http://localhost:3000/public/images/user/'+fileName
+      })
+    } else {
+      res.json({
+        status: -1,
+        msg: '上传的文件名有误'
+      })
+    }
+    //
+
+    //如果提交文件的form中将上传文件的input名设置为file，就从file中取上传文件。否则取for in循环第一个上传的文件。  
+    //   if (file.file) {
+    //     filePath = file.file.path;
+    //   } else {
+    //     for (var key in file) {
+    //       if (file[key].path && filePath === '') {
+    //         filePath = file[key].path;
+    //         break;
+    //       }
+    //     }
+    //   }
+    //   //文件移动的目录文件夹，不存在时创建目标文件夹  
+    //   var targetDir = path.join(__dirname+'/../public/images/', 'user');
+    //   if (!fs.existsSync(targetDir)) {
+    //     fs.mkdir(targetDir);
+    //   }
+    //   var fileExt = filePath.substring(filePath.lastIndexOf('.'));
+    //   //判断文件类型是否允许上传  
+    //   if (('.jpg.jpeg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
+    //     var err = new Error('此文件类型不允许上传');
+    //     res.json({
+    //       code: -1,
+    //       message: '此文件类型不允许上传'
+    //     });
+    //   } else {
+    //     //以当前时间戳对上传文件进行重命名  
+    //     var fileName = new Date().getTime() + fileExt;
+    //     var targetFile = path.join(targetDir, fileName);
+    //     //移动文件  
+    //     fs.rename(filePath, targetFile, function (err) {
+    //       if (err) {
+    //         console.info(err);
+    //         res.json({
+    //           code: -1,
+    //           message: '操作失败'
+    //         });
+    //       } else {
+    //         //上传成功，返回文件的相对路径  
+    //         var fileUrl = 'http://localhost:3000/public/images/user/' + fileName;
+    //         res.json({
+    //           code: 0,
+    //           fileUrl: fileUrl
+    //         });
+    //       }
+    //     });
+    //   }
   });
 });
 
